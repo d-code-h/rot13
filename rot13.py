55bcf63259487f0ab24217847d921b5afe2968ee
Habeeb David <habeebdh1@gmail.com>
2022-06-13 12:26:00 +0000

    simplify codes

--- rot13.py
+++ rot13.py
@@ -1,22 +1,21 @@
 import sys
-upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+up = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+low = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-rot13 = ''
-rot = sys.argv[1]
-for each in rot:
-  if each.isupper():
-    i = upper.index(each)
+rot = ''
+for e in sys.argv[1]:
+  if e.isupper():
+    i = up.index(e)
     i += 13
-    while i >= len(upper):
-      i = i - len(upper)
-    rot13 += upper[i]
-  elif each.islower():
-    i = lower.index(each)
+    while i >= len(up):
+      i = i - len(up)
+    rot += up[i]
+  elif e.islower():
+    i = low.index(e)
     i += 13
-    while i >= len(lower):
-      i = i - len(lower)
-    rot13 += lower[i]
+    while i >= len(low):
+      i = i - len(low)
+    rot += low[i]
   else:
-    rot13 += each
-print(rot13)
+    rot += e
+print(rot)
